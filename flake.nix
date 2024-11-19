366f6b75a6795bef442cb1e35223b867e710aee5
rukh-debug <git@rubenk.dev>
2024-11-19 12:49:50 -0500

    fix: now uses platform and remove unnecessary/irrivelant fetchFromGithub

--- flake.nix
+++ flake.nix
@@ -8,41 +8,43 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     let
-      systems = flake-utils.lib.systems.defaults;
+      systems = lib.platforms.all;
+      lib = nixpkgs.lib;
     in
     {
-      packages = flake-utils.lib.eachDefaultSystem (system:
+      packages = lib.genAttrs systems (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
           lib = pkgs.lib;
-        in {
+        in
+        {
           default = pkgs.rustPlatform.buildRustPackage rec {
             pname = "plz-cli";
             version = "0.1.0";
 
-            src = pkgs.fetchFromGitHub {
-              owner = "rukh-debug";
-              repo = "plz-cli";
-              rev = "11cda7cd024004107c689b5f44ec1d6471c0fcd5";
-              sha256 = "sha256-fW5Kp74PByTQ3uhxstkXy/tNNtTIgjh26px30QHgCgo=";
-            };
+            src = self;
 
-            cargoHash = "sha256-YOsZAXgaklfSNj3wbL4IgUTs6hLOIjwG5Ij36bDhtFQ=";
+            # cargoHash = null;
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
 
             meta = with lib; {
               description = "plz-cli tool";
               homepage = "https://github.com/rukh-debug/plz-cli";
               license = licenses.mit;
-              mainProgram = "plz";  # Important for CLI tools
+              mainProgram = "plz"; # Important for CLI tools
             };
+
           };
         }
       );
 
-      devShells = flake-utils.lib.eachDefaultSystem (system:
+      devShells = lib.genAttrs systems (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-        in {
+        in
+        {
           default = pkgs.mkShell {
             buildInputs = [ self.packages.${system}.default ];
           };
@@ -54,5 +56,6 @@
           environment.systemPackages = [ self.packages.x86_64-linux.default ];
         };
       };
+
     };
 }
